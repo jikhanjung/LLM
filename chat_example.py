80a6b83e5b1f96a9aaa05f99101af1bfeab58926
Jikhan Jung <honestjung@gmail.com>
2023-11-21 06:15:29 +0000

    ztest2 modified

--- chat_example.py
+++ chat_example.py
@@ -1,19 +1,16 @@
 # Code refactored from https://docs.streamlit.io/knowledge-base/tutorials/build-conversational-apps
 
-import openai
-import streamlit as st
 
-with st.sidebar:
-    st.title('🤖💬 OpenAI Chatbot')
-    if 'OPENAI_API_KEY' in st.secrets:
-        st.success('API key already provided!', icon='✅')
-        openai.api_key = st.secrets['OPENAI_API_KEY']
-    else:
-        openai.api_key = st.text_input('Enter OpenAI API token:', type='password')
-        if not (openai.api_key.startswith('sk-') and len(openai.api_key)==51):
-            st.warning('Please enter your credentials!', icon='⚠️')
-        else:
-            st.success('Proceed to entering your prompt message!', icon='👉')
+from openai import OpenAI
+import streamlit as st
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+client=OpenAI()
+
+
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
@@ -29,11 +26,12 @@ if prompt := st.chat_input("What is up?"):
     with st.chat_message("assistant"):
         message_placeholder = st.empty()
         full_response = ""
-        for response in openai.ChatCompletion.create(
+        for response in client.chat.completions.create(
             model="gpt-3.5-turbo",
             messages=[{"role": m["role"], "content": m["content"]}
                       for m in st.session_state.messages], stream=True):
-            full_response += response.choices[0].delta.get("content", "")
+            full_response += str(response.choices[0].delta.content)
+            #full_response += response.choices[0].delta.get("content", "")
             message_placeholder.markdown(full_response + "▌")
         message_placeholder.markdown(full_response)
     st.session_state.messages.append({"role": "assistant", "content": full_response})
